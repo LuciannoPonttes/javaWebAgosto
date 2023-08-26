282f4aaa4c8ccbb95ce9d3ba48003f1a7df18c3b
luciannoponttes <luciannoponttes@gmail.com>
2023-08-25 22:44:37 -0300

    spring boot v2

--- projetoFinal/projetoDespesa/src/main/java/projetoDespesa/projetoDespesa/controller/DespesaController.java
+++ projetoFinal/projetoDespesa/src/main/java/projetoDespesa/projetoDespesa/controller/DespesaController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import projetoDespesa.projetoDespesa.model.Despesa;
 import projetoDespesa.projetoDespesa.repository.DespesaRepositorio;
@@ -30,6 +31,21 @@ public class DespesaController {
 		despesaRepositorio.save(despesa);
 		
 		return "redirect:/despesa";
+	}
+	
+	
+	@RequestMapping(value="/despesasLista", method=RequestMethod.GET)
+	public ModelAndView listarDespesas() {
+		
+		ModelAndView modelAndView = new ModelAndView("listaDespesas"); // html que vai acessado
+		Iterable<Despesa> listaDespesas = despesaRepositorio.findAll();
+		
+		modelAndView.addObject("listaDespesas", listaDespesas);//lista
+		
+		
+		
+		
+		return modelAndView;
 	}
 	
 	
